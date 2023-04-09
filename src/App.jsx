cbae8cb05b1aa041516e6c12c156d8c0acffd597
ayoub <ayoubousbia02@gmail.com>
2023-04-09 10:05:05 +0200

    fix heart not in the boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import './App.css';
+import { useState } from "react"
+import AnimalShow from "./AnimalShow"
+
+function getRandomAnimal() {
+    const animals = ["bird", "cat", "dog", "horse", "gator", "cow"]
+
+    return animals[Math.floor(Math.random() * animals.length)]
+}
+
 function App() {
+    const [animals, setAnimals] = useState([])
+
+
     const handleClick = () => {
-        alert("hi")
+        setAnimals([...animals, getRandomAnimal()])
     }
 
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow key={index} type={animal} />
+    })
+
     return (
-        <div>
+        <div className="app">
             <button onClick={handleClick}> Add animal</button>
+            <div className="animal-list">{renderedAnimals}</div>
         </div>
     )
 }
